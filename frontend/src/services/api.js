0b9100b1524766d25860335f5306c0b2cc3fd93e
abdullah <mamalik@systellex.com>
2024-12-30 23:13:20 +0500

    api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://video-sharing-backend-ewgnfnepbcamg3bq.uksouth-01.azurewebsites.net/api',
 });
 
 export default instance;
